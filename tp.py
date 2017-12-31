49000cc6cd6af3f8e28c75ebcf37157bf0c3a9fa
Xulz <xulz@qq.com>
2017-12-31 09:53:23 +0800

    Site updated: 2017-12-31 09:53:23

---
+++ tp.py
@@ -0,0 +1,41 @@
+def handle_photo():
+    '''根据图片的文件名处理成需要的json格式的数据
+    
+    -----------
+    最后将data.json文件存到博客的source/photos文件夹下
+    '''
+    src_dir, des_dir = "photos/", "min_photos/"
+    file_list = list_img_file(src_dir)
+    list_info = []
+    for i in range(len(file_list)):
+        filename = file_list[i]
+        date_str, info = filename.split("_")
+        info, _ = info.split(".")
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        year_month = date_str[0:7]            
+        if i == 0:  # 处理第一个文件
+            new_dict = {"date": year_month, "arr":{'year': date.year,
+                                                                   'month': date.month,
+                                                                   'link': [filename],
+                                                                   'text': [info],
+                                                                   'type': ['image']
+                                                                   }
+                                        } 
+            list_info.append(new_dict)
+        elif year_month != list_info[-1]['date']:  # 不是最后的一个日期，就新建一个dict
+            new_dict = {"date": year_month, "arr":{'year': date.year,
+                                                   'month': date.month,
+                                                   'link': [filename],
+                                                   'text': [info],
+                                                   'type': ['image']
+                                                   }
+                        }
+            list_info.append(new_dict)
+        else:  # 同一个日期
+            list_info[-1]['arr']['link'].append(filename)
+            list_info[-1]['arr']['text'].append(info)
+            list_info[-1]['arr']['type'].append('image')
+    list_info.reverse()  # 翻转
+    final_dict = {"list": list_info}
+    with open("../data.json","w") as fp:
+        json.dump(final_dict, fp)
