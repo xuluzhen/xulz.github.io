70f7d404678567d84e3617a4ba46f25941c3d02f
Xulz <xulz@qq.com>
2019-07-24 15:08:31 +0800

    Site updated: 2019-07-24 15:08:31

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/xuluzhen/blog-photo/master/mini_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/xuluzhen/blog-photo/master/photos/' + data.link[i];
+          var minSrc = 'http://106.75.26.43/blog-photo/mini_photos/' + data.link[i];
+          var src = 'http://106.75.26.43/blog-photo/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
